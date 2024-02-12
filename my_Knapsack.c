2b60d759c19fb6f92c319543860ea70c00c9231e
linoy <kaufmanlinoy@gmail.com>
2024-02-12 16:48:06 +0200

    add

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -1,10 +1,19 @@
- 
-int main {
+ #include <stdio.h>
+ #define maxitem 5;
+ #define maxweight 20;
 
+int main() {
+int names [5];
+int values [5];
+int weights[5];
     for (int i = 0; i < 5; i++)
     {
-        printf("name of item" ,)
-        scanf()
+        printf("name of the item" ,);
+        scanf("%s", &names[i]);
+        printf("value of the item" ,);
+        scanf("%d", &values[i]);
+        printf("weight of the item" ,);
+        scanf("%d", &weights[i]);
     }
     
 }
