1984f218cfc57335545617994514e8dcdb94a64b
linoy <kaufmanlinoy@gmail.com>
2024-02-13 11:29:11 +0200

    add

--- makefile
+++ makefile
@@ -1,15 +1,28 @@
-.PHONY : 
-all: clean
+.PHONY : all clean
+all: my_graph my_Knapsack
+FLAGS= -g -Wall
 
 clean :
 	rm *.o
+	rm my_graph
+	rm my_Knapsack
+	rm * .a
+	rm -f
 
+my_Knapsack : my_Knapsack.o
+	gcc -o my_Knapsack my_Knapsack.o
 
+my_Knapsack.o: my_Knapsack.c 
+	gcc $(FLAGS) -c my_Knapsack.c -o	my_Knapsack.o
 
 my_graph : my_graph.o my_mat.o
+	gcc $(FLAGS)  my_graph.o my_mat.o -o my_graph
 
 my_graph.o : my_graph.c my_mat.h
-	gcc -c  my_graph.c -o my_graph.o 
+	gcc $(FLAGS) -c my_graph.c 
 
 my_mat.o : my_mat.c my_mat.h
-	gcc -c my_mat.c -o my_mat.o 
+	gcc $(FLAGS) -fPIC -c my_mat.c 
+
+my_mat.a: my_mat.o
+	ar -rcs my_mat.a my_mat.o
--- my_Knapsack.c
+++ my_Knapsack.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
 #define maxitem 5;
 #define maxweight 20;
 int min(int a, int b)
@@ -11,23 +11,51 @@ int max(int a, int b)
     return (a > b) ? a : b;
 }
 
-int knapSack(int weights[],int values[],int selected_bool[])
+int knapSack(int weights[], int values[], int selected_bool[])
 {
 
-int mat [maxitem+1][maxweight+1];
+    int mat[maxitem + 1][maxweight + 1];
 
-for(int i=0;i<=maxitem;i++)
-{
-
-}
-
+    for (int i = 0; i <= maxitem; i++)
+    {
+        for (int j = 0; j <= maxweight; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                mat[i][j] = 0;
+            }
+            else if (weight[i - 1] <= j)
+            {
+                mat[i][j] = max(mat[i - 1][j], mat[i - 1][j - weight[i - 1]] + values[i - 1]);
+            }
+            else
+            {
+                mat[i][j] = mat[i - 1][j]
+            }
+        }
+    }
+    int j = maxweight;
+    int i = maxitem- 1;
+    int maxvalue = 0;
+    while (i >= 0 && j > 0)
+    {
+        if (mat[i][j] < values[i] + mat[i][j - weights[i]])
+        {
+            selected_bool[i] = 1;
+            j = j - weights[i];
+            maxvalue =  maxvalue + values[i];
+        }
+        i--;
+    }
+    return maxvalue;
 }
 int main()
 {
-    int names[5];
-    int values[5];
-    int weights[5];
-    for (int i = 0; i < 5; i++)
+    int selected_bool[maxitem ] = {0}
+    int names[maxitem ];
+    int values[maxitem ];
+    int weights[maxitem ];
+    for (int i = 0; i < maxitem ; i++)
     {
         printf("name of the item");
         scanf("%s", &names[i]);
@@ -36,6 +64,15 @@ int main()
         printf("weight of the item");
         scanf("%d", &weights[i]);
     }
-
-
+    printf("Maximum profit: ");
+    printf("%d",knapSack(weights,values,selected_bool));
+    printf("\nSelected items:");
+    for(int i = 0; i < maxitem; i++)
+    {
+        if(selected_bool[i] == 1)
+        {
+            printf(" %c",names[i]);
+        }
+    }
+    return 0;
 }
